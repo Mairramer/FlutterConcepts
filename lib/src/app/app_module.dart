86b16e5bcad746e0c3ed6b6ce01f20c59e2ddabc
Mairramer <mairramer.dasilva28@hotmail.com>
2020-09-14 21:31:19 -0300

    shared_preferences adicionado

--- lib/src/app/app_module.dart
+++ lib/src/app/app_module.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gofast/src/app/app_controller.dart';
 import 'package:flutter_gofast/src/app/app_widget.dart';
+import 'package:flutter_gofast/src/core/interfaces/shared_repository_interface.dart';
+import 'package:flutter_gofast/src/core/repositories/shared_repository.dart';
 import 'package:flutter_gofast/src/modules/home/home_module.dart';
 import 'package:flutter_gofast/src/modules/splash/splash_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppModule extends MainModule {
   @override
-  List<Bind> get binds => [Bind((i) => AppController())];
+  List<Bind> get binds => [
+        Bind((i) => AppController()),
+        Bind<ISharedPrederenceInterface>((i) => SharedRepository())
+      ];
 
   @override
   Widget get bootstrap => AppWidget();
---
+++ lib/src/core/interfaces/shared_repository_interface.dart
@@ -0,0 +1,5 @@
+abstract class ISharedPrederenceInterface {
+  setInstance();
+  dynamic getValue<T>(String key);
+  Future<bool> setValue<T>(String key, dynamic value);
+}
