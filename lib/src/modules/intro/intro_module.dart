33afac10ec320c28a93e41a88b70da756dfb26f5
Mairramer <mairramer.dasilva28@hotmail.com>
2020-10-09 20:52:07 -0300

    intro module

--- lib/src/modules/intro/intro_module.dart
+++ lib/src/modules/intro/intro_module.dart
@@ -1,11 +1,14 @@
+import 'package:flutter_gofast/src/modules/intro/intro_controller.dart';
+import 'package:flutter_gofast/src/modules/intro/intro_page.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class IntroModule extends ChildModule {
   @override
-  // TODO: implement binds
-  List<Bind> get binds => null;
+  List<Bind> get binds => [Bind((i) => IntroController())];
 
   @override
-  // TODO: implement routers
-  List<ModularRouter> get routers => null;
+  List<ModularRouter> get routers =>
+      [ModularRouter('/login', child: (_, args) => IntroPage())];
+
+  static Inject get to => Inject<IntroModule>.of();
 }
