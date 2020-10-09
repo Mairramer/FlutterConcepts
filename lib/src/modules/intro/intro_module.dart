824062c50420e6065fe198e372b540a5d243a558
Mairramer <mairramer.dasilva28@hotmail.com>
2020-10-09 20:44:51 -0300

    verificação de  dados

---
+++ lib/src/modules/intro/intro_module.dart
@@ -0,0 +1,11 @@
+import 'package:flutter_modular/flutter_modular.dart';
+
+class IntroModule extends ChildModule {
+  @override
+  // TODO: implement binds
+  List<Bind> get binds => null;
+
+  @override
+  // TODO: implement routers
+  List<ModularRouter> get routers => null;
+}
